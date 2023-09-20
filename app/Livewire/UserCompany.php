ec4a4a65e76886cb4bbb1c8d4b49a25eb37fb8e9
Victor Dracxler <victordracxler@poli.ufrj.br>
2023-09-20 17:57:17 -0300

    feat: pegar user e company Id OK

--- app/Livewire/UserCompany.php
+++ app/Livewire/UserCompany.php
@@ -12,21 +12,32 @@ use Livewire\Attributes\Layout;
 
 class UserCompany extends Component
 {
-    public $companyList;
-    public $usersList;
+    public $companyList, $company, $selectedCompany;
+    public $usersList, $user, $selectedUser;
+
     
-    public $user, $selectedUser;
 
     public function mount()
     {
-        $this-> companyList = CompanyModel::all();
-        $this-> usersList = User::all();
+        $this->companyList = CompanyModel::all();
+        $this->usersList = User::all();
 
     }
 
-    public function updateUserCompanyId($userId, $companyId){
+    public function updateUserCompanyId($userId, $companyId)
+    {
 
     }
+
+    public function handleSelectChangeUser()
+    {      
+        $this->selectedUser = User::find($this->selectedUser)->id;
+    }
+    public function handleSelectChangeCompany()
+    {      
+        $this->selectedCompany = CompanyModel::find($this->selectedCompany)->id;
+    }
+
     public function render()
     {
         return view('livewire.user-company');
--- resources/views/livewire/user-company.blade.php
+++ resources/views/livewire/user-company.blade.php
@@ -9,24 +9,37 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-20" style="color: white">
 
         <h2 style="font-size: 20px">Qual usuário pertence a qual empresa</h2>
-        
+
         <div>
             <label for="user">Selecione um usuário(label):</label>
-            <select wire:model="selectedUser" style="color: gray">
+            <select wire:model="selectedUser" style="color: gray" wire:change="handleSelectChangeUser">
                 <option value="">Selecione um usuário(placeholder)</option>
                 @foreach ($usersList as $id => $user)
-                    <option value="{{ $user->id}}">{{ $user->name }}</option>
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select>
-        
-            <h1>{{$selectedUser }}</h1>
-            @if ($selectedUser)
-                <p>Você selecionou: {{ $usersList[$selectedUser] }}</p>
-            @endif
-        </div>
-        
 
-        
+            <h1>Isso é o selected user Id: {{ $selectedUser }}</h1>
+
+           
+
+        </div>
+
+        <div>
+            <label for="user">Selecione uma empresa(label):</label>
+            <select wire:model="selectedCompany" style="color: gray" wire:change="handleSelectChangeCompany">
+                <option value="">Selecione uma empresa(placeholder)</option>
+                @foreach ($companyList as $id => $company)
+                    <option value="{{ $company->id }}">{{ $company->razao }}</option>
+                @endforeach
+            </select>
+
+            <h1>Isso é o selected company Id: {{ $selectedCompany }}</h1>
+
+           
+        </div>
+
+
 
     </div>
 
